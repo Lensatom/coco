2a289134606b07daa47684bf557862aa72cd7609
Lensatom <77001715+Lensatom@users.noreply.github.com>
2021-11-23 12:05:32 +0100

    Update newAccount.js

--- pages/newAccount.js
+++ pages/newAccount.js
@@ -3,25 +3,7 @@ import { useRouter } from 'next/router'
 import Link from 'next/link'
 import styles from '../styles/NewAccount.module.css'
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyAVbzVOFHbh1umJ7RQ1dtI2DvzhHnF5GJ8",
-    authDomain: "coco-dabda.firebaseapp.com",
-    projectId: "coco-dabda",
-    storageBucket: "coco-dabda.appspot.com",
-    messagingSenderId: "999785564311",
-    appId: "1:999785564311:web:089121734995c15b51c1d2",
-    measurementId: "G-HBL34MY9G0"
-};
-
-const database = `${process.env.DB}/user`
+const database = "https://coco-dabda-default-rtdb.firebaseio.com/"
 
 function Account({ posts }) {
     
